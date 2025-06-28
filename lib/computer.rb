2a2117d1ec1df5cfd01b76a3a115b2fb0286d08c
Yeet7 <51092112+Yeet7@users.noreply.github.com>
2025-06-28 00:20:19 -0400

    Create basic classes & their initialize methods

---
+++ lib/computer.rb
@@ -0,0 +1,13 @@
+# Create class for computer player.
+# This class will generate a random code for the game.
+# It will also be able to provide feedback on the player's guesses.
+
+# The computer will generate a random code of 4 colors from a set of 6 colors.
+# The colors are represented by letters: R, G, B, Y, O, P (Red, Green, Blue, Yellow, Orange, Purple).
+
+# Example usage:
+# computer = Computer.new
+# computer.generate_code
+class Computer
+  def initialize(args); end
+end
